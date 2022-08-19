cd6f444641f5d56e0495ad6cec90a951afbe8938
DaniPinelli <danielpinelli.fullstack@gmail.com>
2022-08-19 16:15:27 -0300

    First steps

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const http = require('http');
+
+http.createServer((req, res) => {
+
+    res.setHeader('Content-Disposition', 'attachment; filename="test.csv"');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write("id, nombre\n");
+    res.write("1, Juan\n");
+    res.write("2, Pedro\n");
+    res.write("3, Lucas\n");
+    res.write("4, Marcos\n");
+    res.write("5, Mateo\n");
+    res.end();
+})
+    .listen(8080);
+
+console.log('Server running at http://localhost:8080/');
