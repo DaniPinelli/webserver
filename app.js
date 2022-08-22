40e225f285206b110218c9a78c93e1502f23d95d
DaniPinelli <danielpinelli.fullstack@gmail.com>
2022-08-22 10:40:57 -0300

    webserver ready

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 const express = require('express')
+const hbs = require('hbs');
+require('dotenv').config();
+
 const app = express()
-const port = 8080;
+const port = process.env.PORT;
 
-
+//Handlebars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 //Middleware to serve static files
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home', {
+        title: 'Home',
+        message: 'Welcome to the home page'
+    })
 });
 
 app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html')
+    res.render('generic', {
+        title: 'Home',
+        message: 'Welcome to the home page'
+    })
 });
 
 app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html')
+    res.render('elements', {
+        title: 'Home',
+        message: 'Welcome to the home page'
+    })
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html')
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(__dirname + '/public/404.html')
+// });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
